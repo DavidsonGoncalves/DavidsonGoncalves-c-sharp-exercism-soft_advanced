fdbc1f9bbc3343ce33d3a93c92a1bda36ae3f9c7
Davidson Goncalves <davidson.cg7@gmail.com>
2021-08-20 11:52:35 -0300

    Collors of a resistor

---
+++ resistor-color - soft/ResistorColor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public static class ResistorColor
+{
+    public static int ColorCode(string color)
+    {
+        int code=0;
+        for(int i =0; i<Colors().Length; i++)
+        {
+            if(Colors()[i]==color)
+            {
+                code = i;
+            }
+        }
+        return code;
+    }
+
+    public static string[] Colors()
+    {
+      string[] colors = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "violet", "grey", "white"};
+      return colors;
+    }
+}
