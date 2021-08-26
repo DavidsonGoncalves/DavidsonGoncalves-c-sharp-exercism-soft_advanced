daf0a1561669f5d9fe58408a5098108af0b2fed9
Davidson Goncalves <davidson.cg7@gmail.com>
2021-08-26 09:31:46 -0300

    Perfect numbers

---
+++ perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        Classification what =  new Classification();
+        int total=0;
+        
+        for(int i=0; i<=number;i++)
+        {
+            if(i % number==0)
+            {
+                total+=i;   
+            }
+
+        }
+         if(total==number){
+            what = Classification.Perfect;
+         }
+         else 
+         if(total>number){
+             what = Classification.Abundant;
+         }
+         else 
+             what = Classification.Deficient;
+         
+        return what;
+    }
+}
